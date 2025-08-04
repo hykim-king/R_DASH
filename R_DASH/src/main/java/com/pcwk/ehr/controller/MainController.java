fe47335983dc357a5319358c23c320be99e15a21
gyeong ryung <zoqwkdrudfud@gmail.com>
2025-08-04 15:59:12 +0900

    fighting

--- R_DASH/src/main/java/com/pcwk/ehr/controller/MainController.java
+++ R_DASH/src/main/java/com/pcwk/ehr/controller/MainController.java
@@ -9,27 +9,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    final Logger log = LogManager.getLogger(getClass());
+    private static final Logger log = LogManager.getLogger(MainController.class);
 
     public MainController() {
-        log.debug("┌─────────────────────────────────┐");
-        log.debug("│ MainController() 생성                            │");
-        log.debug("└─────────────────────────────────┘");
+        log.debug("┌──────────────────────────────────────────────┐");
+        log.debug("│ MainController() 생성자 호출                           │");
+        log.debug("└──────────────────────────────────────────────┘");
     }
 
     /**
-     * 메인 페이지 출력
-     * http://localhost:8080/ehr/home
+     * 메인 화면 출력
+     * 요청 URL: http://localhost:8080/ehr/home
+     * @param model
+     * @return main.jsp
      */
     @GetMapping("/home")
     public String home(Model model) {
         log.debug("┌─────────────────────────────┐");
-        log.debug("│ *home()*                    │");
+        log.debug("│ home() 메서드 실행             │");
         log.debug("└─────────────────────────────┘");
 
-        model.addAttribute("msg", "재난 알림 웹사이트 '재민이'에 오신 것을 환영합니다!");
+        // 메시지를 JSP로 전달
+        model.addAttribute("msg", "저희 재난 알림 사이트를 방문해주셔서 감사합니다.");
 
-        return "main"; // → /WEB-INF/views/main.jsp
+        return "main/main"; // → /WEB-INF/views/main/main.jsp
     }
-
 }
