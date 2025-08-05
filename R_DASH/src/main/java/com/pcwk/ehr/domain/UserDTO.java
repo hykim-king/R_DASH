4d7f77632e57a03da3cfef88672e54766c2bb974
koo <qhsghddl33@naver.com>
2025-08-05 23:47:27 +0900

    userdaotest 진행중

--- R_DASH/src/main/java/com/pcwk/ehr/domain/UserDTO.java
+++ R_DASH/src/main/java/com/pcwk/ehr/domain/UserDTO.java
@@ -2,27 +2,27 @@ package com.pcwk.ehr.domain;
 
 import com.pcwk.ehr.cmn.DTO;
 
-public class UserDTO extends DTO{
-	private String userNo; // 회원번호
+public class UserDTO extends DTO {
+	private int userNo; // 회원번호
 	private String password; // 비밀번호
 	private String name; // 이름
 	private String nickname; // 닉네임
 	private String image; // 이미지
 	private String email; // 이메일
 	private String tel; // 전화번호
-	private String zip_code; // 우편번호
+	private int zipCode; // 우편번호
 	private String address; // 주소
 	private String detailAddress; // 상세 주소
-	private String role; // 권한 구분
+	private int role; // 권한 구분
 	private String regDt; // 등록일
 
 	public UserDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public UserDTO(String userNo, String password, String name, String nickname, String image, String email, String tel,
-			String zip_code, String address, String detailAddress, String role, String regDt) {
+
+	public UserDTO(int userNo, String password, String name, String nickname, String image, String email, String tel,
+			int zipCode, String address, String detailAddress, int role, String regDt) {
 		super();
 		this.userNo = userNo;
 		this.password = password;
@@ -31,18 +31,18 @@ public class UserDTO extends DTO{
 		this.image = image;
 		this.email = email;
 		this.tel = tel;
-		this.zip_code = zip_code;
+		this.zipCode = zipCode;
 		this.address = address;
 		this.detailAddress = detailAddress;
 		this.role = role;
 		this.regDt = regDt;
 	}
 
-	public String getUserNo() {
+	public int getUserNo() {
 		return userNo;
 	}
 
-	public void setUserNo(String userNo) {
+	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
 
@@ -94,12 +94,12 @@ public class UserDTO extends DTO{
 		this.tel = tel;
 	}
 
-	public String getZip_code() {
-		return zip_code;
+	public int getZipCode() {
+		return zipCode;
 	}
 
-	public void setZip_code(String zip_code) {
-		this.zip_code = zip_code;
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
 	}
 
 	public String getAddress() {
@@ -118,11 +118,11 @@ public class UserDTO extends DTO{
 		this.detailAddress = detailAddress;
 	}
 
-	public String getRole() {
+	public int getRole() {
 		return role;
 	}
 
-	public void setRole(String role) {
+	public void setRole(int role) {
 		this.role = role;
 	}
 
@@ -137,7 +137,7 @@ public class UserDTO extends DTO{
 	@Override
 	public String toString() {
 		return "UserDTO [userNo=" + userNo + ", password=" + password + ", name=" + name + ", nickname=" + nickname
-				+ ", image=" + image + ", email=" + email + ", tel=" + tel + ", zip_code=" + zip_code + ", address="
+				+ ", image=" + image + ", email=" + email + ", tel=" + tel + ", zipCode=" + zipCode + ", address="
 				+ address + ", detailAddress=" + detailAddress + ", role=" + role + ", regDt=" + regDt + "]";
 	}
 
