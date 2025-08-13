89cbf5cec23bbc4fc2d01fb8d4250e1b0b359f09
김승재 <yejiad12@gmail.com>
2025-08-13 13:52:17 +0900

    이름 변경

--- R_DASH/src/main/java/com/pcwk/ehr/controller/TemperatureController.java
+++ R_DASH/src/main/java/com/pcwk/ehr/controller/TemperatureController.java
@@ -1,20 +1,18 @@
 package com.pcwk.ehr.controller;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.pcwk.ehr.domain.NowcastDTO;
 import com.pcwk.ehr.domain.PatientsDTO;
 import com.pcwk.ehr.service.TemperatureService;
 
@@ -24,6 +22,21 @@ public class TemperatureController {
 
     @Autowired
     private TemperatureService temperatureService;
+    
+    @GetMapping("/summary")
+    @ResponseBody
+    public List<PatientsDTO> getPatientsSummary(
+            @RequestParam(required = false) String groupType,
+            @RequestParam(required = false) String year,
+            @RequestParam(required = false) String sidoNm) throws SQLException {
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("groupType", groupType);
+        param.put("year", year);
+        param.put("sidoNm", sidoNm);
+
+        return temperatureService.selectPatientsSummary(param);
+    }
     
     @PostMapping("/save-patients.do")
     @ResponseBody
@@ -47,7 +60,7 @@ public class TemperatureController {
 
     @GetMapping("/main.do")
     public String main() {
-        return "temperature/main";
+        return "stats/main";
     }
     
 }
